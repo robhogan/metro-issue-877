cb773b5a128e54d134dc0f5b4ff705b38d0cdb20
Rob Hogan <rob@rhogan.net>
2022-10-02 18:07:21 +0100

    Repro

---
+++ index.js
@@ -0,0 +1,34 @@
+const { transformSync } = require("@babel/core");
+
+const SRC = `
+class BaseClass {
+  constructor() {
+    this.customProp = "foo";
+  }
+}
+class ChildClass extends BaseClass {
+  customProp: ?string;
+}
+const child = new ChildClass();
+console.log(child.customProp);
+`
+
+const preset = {
+  plugins: [
+    "@babel/plugin-transform-flow-strip-types",
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
+
+const withPlugin = transformSync(SRC, {
+  presets: [preset],
+  plugins: ["@babel/plugin-proposal-class-properties"],
+}).code;
+
+const withoutPlugin = transformSync(SRC, {
+  presets: [preset],
+  plugins: [],
+}).code;
+
+eval(withPlugin);
+eval(withoutPlugin);
